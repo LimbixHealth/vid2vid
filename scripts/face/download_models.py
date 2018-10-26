f186dbfe701fb392a1d3669f796872451bf0cc9b
Dustin Freeman <dustin.freeman@gmail.com>
2018-10-26 02:16:07 +0000

    Handle python scripting paths. A bit awkward

--- scripts/face/download_models.py
+++ scripts/face/download_models.py
@@ -1,4 +1,10 @@
 import os
+
+#the following two lines allow this script to be called from the vid2vid repo root
+#this is awkward. Discussion here: https://github.com/NVIDIA/vid2vid/issues/36#issuecomment-432907265
+import sys
+sys.path.insert(0, '/vid2vid/')
+
 from scripts.download_gdrive import *
 
 file_id = '10LvNw-2lrh-6sPGkWbQDfHspkqz5AKxb'
